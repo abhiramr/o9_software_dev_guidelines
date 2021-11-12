c8e040810269585e53bafb3077839cce952b859d
Abhiram R <abhi.darkness@gmail.com>
2021-11-12 08:34:58 +0530

    Added updated code

--- o9_dummy_folder/bad_foo.py
+++ o9_dummy_folder/bad_foo.py
@@ -1,3 +1,14 @@
+import numpy as np
+
+class Point:
+    def __init__(self, a,b,c):
+        self.a = a 
+        self.b = b
+        self.c = c
+    
+    def get_point(self):
+        return np.array((self.a, self.b, self.c))
+
 class BadFoo:
     def __init__(self, a, b):
         self.a = a
@@ -8,4 +19,9 @@ class BadFoo:
 
     def sub(self):
         return self.a - self.b
+
+    def dist(self, p1, p2):
+        dist_ = np.linalg.norm(p1 - p2)
+        return dist_
+
     
--- o9_dummy_folder/good_foo.py
+++ o9_dummy_folder/good_foo.py
@@ -1,11 +1,30 @@
 import logging
 from typing import Optional
 
+import numpy as np
+from numpy.core.defchararray import array
+
+class Point:
+    """Returns a 3d-Array tuple of coordinates"""
+    def __init__(self, a,b,c):
+        self.a = a 
+        self.b = b
+        self.c = c
+    
+    def get_point(self):
+        return np.array((self.a, self.b, self.c))
+
 class GoodFoo:
     def __init__(self, a, b):
         self.logger = logging.getLogger("o9_logger")
         self.a = a
         self.b = b
+
+    def point_validity_check(self):
+        """ Checks if both inputs are valid Points for calculating the Euclidean Distance"""
+        if isinstance(self.a, np.ndarray) and isinstance(self.b, np.ndarray):
+            return True
+        return False
 
     def add(self) -> Optional[int]:
         """ Adds two numbers """
@@ -19,7 +38,8 @@ class GoodFoo:
     def sub(self) -> Optional[int]:
         """ Subtracts two numbers """
         try:
-            return self.a - self.b
+            _difference = self.a - self.b
+            return _difference
         except Exception as e:
             self.logger.exception("Error in subtracting two numbers - {}".format(e))
             return None
@@ -27,7 +47,8 @@ class GoodFoo:
     def mul(self) -> Optional[int]:
         """ Multiplies two numbers """
         try:
-            return self.a * self.b
+            _product = self.a * self.b
+            return _product
         except Exception as e:
             self.logger.exception("Error in subtracting two numbers - {}".format(e))
             return None
@@ -36,7 +57,20 @@ class GoodFoo:
     def div(self) -> Optional[float]:
         """ Divides two numbers """
         try:
-            return self.a / self.b
+            _quotient = self.a / self.b
+            return _quotient
         except Exception as e:
             self.logger.exception("Error in dividing two numbers - {}".format(e))
             return None
+
+    def get_euclidean_distance(self) -> Optional[float]:
+        """ Returns the Euclidean Distance between two points """
+        try:
+            if self.point_validity_check():
+                dist_ = np.linalg.norm(self.a - self.b)
+                return dist_
+            self.logger.warning("Input Point validity fail. Euclidean distance will not be calculated.")
+        except Exception as e:
+            self.logger.exception("Error in calculating the euclidean distance - {}".format(e))
+            return None
+        
